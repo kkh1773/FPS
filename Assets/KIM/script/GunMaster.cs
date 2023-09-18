124f2809ceb6154a317b7b08699b1c6f35792efa
kkh1773 <kkh177360@gmail.com>
2023-09-18 16:26:50 +0900

    무기 랜덤으로 가지기

---
+++ Assets/KIM/script/GunMaster.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunMaster : MonoBehaviour
+{
+    public GameObject[] guns;
+    public Animation[] anims;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    
+    
+}
