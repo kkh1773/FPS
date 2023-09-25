03d2837b79f0855c376666091b88f5d54b325a0d
kkh1773 <70687286+kkh1773@users.noreply.github.com>
2023-09-25 12:42:01 +0900

    enemyview

--- Assets/KIM/script/EnemyView.cs
+++ Assets/KIM/script/EnemyView.cs
@@ -9,18 +9,21 @@ public class EnemyView : MonoBehaviour
     [SerializeField] float ViewRadius = 1f;
     [SerializeField] LayerMask TargetMask;
     [SerializeField] LayerMask ObstacleMask;
-    List<Collider> hitTargetList = new List<Collider>();
+    bool ishit=false;
+    [SerializeField]List<Collider> hitTargetList = new List<Collider>();
+    
+    public Transform TelePos;
+    public bool look=false;
+
     // Start is called before the first frame update
     void Start()
     {
-        hitTargetList.Clear();
+        DebugMode=true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
     }
     private void OnDrawGizmos() {
         if (!DebugMode) return;
@@ -30,25 +33,30 @@ public class EnemyView : MonoBehaviour
         Vector3 rightDir = AngleToDir(transform.eulerAngles.y + ViewAngle * 0.5f);
         Vector3 leftDir = AngleToDir(transform.eulerAngles.y - ViewAngle * 0.5f);
         Vector3 lookDir = AngleToDir(lookingAngle);
-
         Debug.DrawRay(myPos, rightDir * ViewRadius, Color.blue);
         Debug.DrawRay(myPos, leftDir * ViewRadius, Color.blue);
         Debug.DrawRay(myPos, lookDir * ViewRadius, Color.cyan);
         
-        Collider[] Targets = Physics.OverlapSphere(myPos, ViewRadius, TargetMask);
+        Collider[] Targets = Physics.OverlapSphere(myPos, ViewRadius,TargetMask);
 
         if (Targets.Length == 0) return;
         foreach(Collider EnemyColli in Targets)
         {
             Vector3 targetPos = EnemyColli.transform.position;
             Vector3 targetDir = (targetPos - myPos).normalized;
+            float targetDis=Vector3.Distance(myPos,targetPos);
             float targetAngle = Mathf.Acos(Vector3.Dot(lookDir, targetDir)) * Mathf.Rad2Deg;
-            if(targetAngle <= ViewAngle * 0.5f && !Physics.Raycast(myPos, targetDir, ViewRadius, ObstacleMask))
+            if(targetAngle <= ViewAngle * 0.5f && !Physics.Raycast(myPos, targetDir, targetDis, ObstacleMask))
             {
+                look=true;
                 hitTargetList.Add(EnemyColli);
+                TelePos=EnemyColli.transform;
                 if (DebugMode) Debug.DrawLine(myPos, targetPos, Color.red);
             }
+            
         }
+        hitTargetList.Clear();
+        
     }
 
     Vector3 AngleToDir(float angle)
@@ -56,6 +64,5 @@ public class EnemyView : MonoBehaviour
         float radian = angle * Mathf.Deg2Rad;
         return new Vector3(Mathf.Sin(radian), 0f, Mathf.Cos(radian));
     }
-    
 
 }
